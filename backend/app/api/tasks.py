d69f99d9856a1056caeb3fd8afbbe76e93c31c60
akkiyolo <akshatshukla069@gmail.com>
2026-01-07 22:03:08 +0530

    auth updated

--- backend/app/api/tasks.py
+++ backend/app/api/tasks.py
@@ -2,12 +2,21 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from typing import List
 from app.core.database import get_db
-from app.core.auth import get_current_user, require_view,require_create,require_delete,require_edit
+from app.core.auth import AuthUser, get_current_user, require_view,require_create,require_delete,require_edit
 from app.models.task import Task
 from app.schemas.task import TaskCreate,TaskResponse,TaskStatus,TaskStatusUpdate,TaskUpdate
 
 router=APIRouter(prefix="/api/tasks",tags=["tasks"])
 
-@router.get(path:"",response_model=List[TaskResponse])
+@router.get(path="",response_model=List[TaskResponse])
 def list_task(user:AuthUser=Depends(require_view),db:Session=Depends(get_db)):
+  tasks=db.query(Task).filter(Task.org_id==user.org_id).all()
+  return tasks
 
+@router.post(path="",response_model=TaskResponse)
+def list_task(
+  task_data:TaskCreate,
+  user:AuthUser=Depends(require_create),db:Session=Depends(get_db)
+  ):
+  tasks=db.query(Task).filter(Task.org_id==user.org_id).all()
+  return tasks
