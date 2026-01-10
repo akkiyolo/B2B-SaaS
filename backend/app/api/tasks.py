0e9177166252651212ed80ed86be80e8b5be9928
akkiyolo <akshatshukla069@gmail.com>
2026-01-10 12:20:00 +0530

    middleware and run commands

--- backend/app/api/tasks.py
+++ backend/app/api/tasks.py
@@ -14,9 +14,92 @@ def list_task(user:AuthUser=Depends(require_view),db:Session=Depends(get_db)):
   return tasks
 
 @router.post(path="",response_model=TaskResponse)
-def list_task(
+def create_task(
   task_data:TaskCreate,
   user:AuthUser=Depends(require_create),db:Session=Depends(get_db)
-  ):
-  tasks=db.query(Task).filter(Task.org_id==user.org_id).all()
-  return tasks
+):
+  task=Task(
+    title=task_data.title,
+    description=task_data.description,
+    status=task_data.status,
+    org_id=user.org_id,
+    created_by=user.user_id
+  )
+  db.add(task)
+  db.commit()
+  db.refresh(task)
+
+  return task
+
+@router.get(path="/{task_id}",response_model=TaskResponse)
+
+def get_task(
+  task_id:str,
+  user:AuthUser=Depends(require_view),
+  db:Session=Depends(get_db)
+
+):
+  task=db.query(Task).filter(
+    Task.id==task_id,
+    Task.org_id==user.org_id,
+  ).first()
+
+  if not task:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="Task not found"
+    )
+  return task
+
+@router.put(path="/{task_id}",response_model=TaskResponse)
+def update_task(
+  task_id:str,
+  task_data:TaskUpdate,
+  user:AuthUser=Depends(require_edit),
+  db:Session=Depends(get_db)
+
+):
+  task=db.query(Task).filter(
+    Task.id==task_id,
+    Task.org_id==user.org_id,
+  ).first()
+
+  if not task:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="Task not found",
+    )
+  
+  if task_data.title is not None:
+    task.title=task_data.title
+  if task_data.description is not None:
+    task.description=task_data.description
+  if task_data.status is not None:
+    task.status=task_data.status
+
+  db.commit()
+  db.refresh(task)
+  return task
+
+@router.delete(path="/{task_id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(
+  task_id:str,
+  task_data:TaskUpdate,
+  user:AuthUser=Depends(require_edit),
+  db:Session=Depends(get_db)
+
+):
+  task=db.query(Task).filter(
+    Task.id==task_id,
+    Task.org_id==user.org_id,
+  ).first()
+
+  if not task:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="Task not found",
+    )
+  
+  db.delete(task)
+  db.commit()
+  return None
